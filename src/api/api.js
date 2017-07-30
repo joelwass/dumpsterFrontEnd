f37a6481a78a95710f40508d2408ce08c94aedbf
Joel Wasserman <joel.wasserman8@gmail.com>
2017-07-30 12:06:03 -0400

    update to endpoint hitting in api

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const base_url = 'http://localhost:3000';
+const base_url = process.env.NODE_ENV === 'prod' ? 'https://dumpster.herokuapp.com/api/v1/' : 'http://localhost:3000';
 
 module.exports = {
 
